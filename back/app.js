402e78ed4d2b539eba66cd412dc1748aebe32892
Taesk <qkrxotjr9508@gmail.com>
2022-12-20 14:44:44 +0900

    cors set

--- back/app.js
+++ back/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const options = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(options));
 
 app.listen(process.env.PORT, () => {
   console.log("server start");
 });
-
-
