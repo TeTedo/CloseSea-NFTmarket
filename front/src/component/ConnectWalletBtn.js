c069fb515d080d9c74ed5a1fdec1804639fe9893
Taesk <qkrxotjr9508@gmail.com>
2022-12-22 22:10:26 +0900

    지갑 버튼 완료

--- front/src/component/ConnectWalletBtn.js
+++ front/src/component/ConnectWalletBtn.js
@@ -1,35 +1,18 @@
-import React, { useEffect } from "react";
-import Web3 from "web3";
+import React from "react";
+import Web3 from "web3/dist/web3.min.js";
 const ConnectWalletBtn = ({ setAccount, setWeb3 }) => {
-  const connectWallet = async () => {
-    try {
-      const web3 = new Web3(Web3.givenProvider);
-      const accounts = await web3.eth.requestAccounts();
-      setAccount(accounts);
-      setWeb3(web3);
-      console.log(accounts, web3);
-    } catch (err) {
-      console.log("User cancelled");
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    // 1
-    window.addEventListener("load", async () => {
-      let web3;
-      // 2
-      if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        // 3
-      } else if (typeof window.web3 !== "undefined") {
-        web3 = new Web3(window.web3.currentProvider);
-      }
-      if (web3) {
-        // 5
-        await web3.eth.requestAccounts();
-      }
+  const getRequestAccount = async () => {
+    const [account] = await window.ethereum.request({
+      method: "eth_requestAccounts",
     });
-  }, []);
+    return account;
+  };
+  const connectWallet = async () => {
+    const account = await getRequestAccount();
+    const web3 = new Web3(window.ethereum);
+    setAccount(account);
+    setWeb3(web3);
+  };
   return <div onClick={connectWallet}>지갑 연결</div>;
 };
 
